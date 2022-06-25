71ad8ba3c604713d3adada97bd6d531d6bf2feeb
Gary <gary.odonoghue@themobilecompany.com>
2022-06-25 21:38:20 +0200

    Initial Commit

---
+++ sunseeker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  sunseeker
+//
+//  Created by gary.odonoghue  on 25/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sunseeker/sunseekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunseekerApp.swift
+//  sunseeker
+//
+//  Created by gary.odonoghue  on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct sunseekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
